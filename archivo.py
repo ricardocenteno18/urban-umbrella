0a89a5cf158db9eab13fddaea3f4dc124e1d2076
ricardocenteno18 <1909036@upy.edu.mx>
2022-11-09 21:21:16 -0600

    create a function

---
+++ archivo.py
@@ -0,0 +1,17 @@
+h:int or float = 8
+b:int or float = 20
+
+
+def calculate_area(h:int or float, b:int or float) -> int or float:
+    """This function calculate the area of a rectangle"""
+    return h * b
+
+
+area = calculate_area(h, b)
+
+print(area)
+
+
+
+
+
